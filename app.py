3c245e58c0309c167d55f84173ad4f87dd90a851
unknown <Aprendiz@BOGDFPCGMF4103.FormacionSena.RED>
2023-08-11 08:58:08 -0500

    migracion"

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 from config import Config
 from flask_migrate import Migrate, migrate
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 #INICIALIZAR EL OBJETO FLASK
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -16,3 +18,21 @@ class Cliente(db.Model):
     username = db.Column(db.String(100))  #para decir que un dato es unico se usa el unique =true y para llaves primarias se usa el primary_key
     password = db.Column(db.String(200))
     email = db.Column(db.String(100),unique=True)
+
+class Producto(db.Model):
+    __tablename__="Producto"
+    id = db.Column(db.Integer,
+                    primary_key = True)      #para tener el dato como entero se usa el db.Column(db.Integer, primary_key = True)
+    nombre = db.Column(db.String(120),
+                         unique=True)  #para decir que un dato es unico se usa el unique =true y para llaves primarias se usa el primary_key
+    precio = db.Column(db.Numeric(precision=10,
+                         scale = 2))
+    imagen = db.Column(db.String(120),
+                         unique=True)
+    
+class Venta(db.Model):
+    __tablename__="ventas"
+    id = db.Column(db.Integer,
+                    primary_key = True)
+    fecha = db.Column(db.DeteTime ,
+                       default = datetime.utcnow ) #Se coloca para que la fecha sea default asi y se coloque la que esta en el sistema 
