43fa234853f7ecdef8807e39ebe2fe041c5ff7aa
unknown <Aprendiz@BOGDFPCGMF4103.FormacionSena.RED>
2023-08-14 10:29:56 -0500

    Formulario de registro

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
-#importaciones de el proyecto 
-from flask import Flask
+#importaciones de el proyecto  dependencias
+from flask import Flask, render_template
 from config import Config
 from flask_migrate import Migrate, migrate
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import SearchField, PasswordField, SubmitField
 
 #INICIALIZAR EL OBJETO FLASK
 app = Flask(__name__)
@@ -11,6 +13,15 @@ app.config.from_object(Config)
 #inicializar el onjeto sqlAlchemy
 db=SQLAlchemy(app)  #tops de datos : string number etc...
 migrate=Migrate(app,db)
+
+#CREAR FORMULARIO DE REGISTRO CLIENTE
+
+class RegistroClienteFrom(FlaskForm):
+    username = SearchField("Nombre de Usuario")
+    email = SearchField("Email")
+    password = PasswordField("password")
+    submit =SubmitField("Registrar Cliente")
+
 #modelos -entidades de el proyeto 
 class Cliente(db.Model):
     __tablename__="clientes"
@@ -20,7 +31,7 @@ class Cliente(db.Model):
     email = db.Column(db.String(100),unique=True)
 
 class Producto(db.Model):
-    __tablename__="Producto"
+    __tablename__="productos"
     id = db.Column(db.Integer,
                     primary_key = True)      #para tener el dato como entero se usa el db.Column(db.Integer, primary_key = True)
     nombre = db.Column(db.String(120),
@@ -34,5 +45,40 @@ class Venta(db.Model):
     __tablename__="ventas"
     id = db.Column(db.Integer,
                     primary_key = True)
-    fecha = db.Column(db.DeteTime ,
+    fecha = db.Column(db.DateTime ,
                        default = datetime.utcnow ) #Se coloca para que la fecha sea default asi y se coloque la que esta en el sistema 
+    cliente_id = db.Column(db.Integer,
+                        db.ForeignKey('clientes.id'))
+
+class Detalle(db.Model):
+    __tablename__="detalles"
+    id = db.Column(db.Integer,
+                    primary_key = True)
+    producto_id = db.Column(db.Integer,
+                          db.ForeignKey('productos.id'))
+    venta_id = db.Column(db.Integer,
+                          db.ForeignKey('ventas.id'))
+    cantidad = db.Column(db.Integer)
+
+@app.route('/clientes/create',
+           methods = ['GET' , 'POST'])
+def crear_cliente():
+    #instanciar formulario
+    form = RegistroClienteFrom()
+    if form.validate_on_submit():
+    #crear el nuevo cliente
+        c = Cliente(username = form.username.data,
+                email = form.email.data,
+                password =form.password.data)
+        db.session.add(c)
+        db.session.commit()
+        return "Cliente Registrado"
+    return render_template('registro.html',
+                           form = form)
+@app.route('/clientes',
+           methods=['GET'])
+def listar():
+    #seleccionar todos los clientes
+    clientes=Cliente.query.all()
+    return render_template('listar.html',
+                 clientes = clientes)
--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 class Config: 
     SQLALCHEMY_DATABASE_URI='mysql+pymysql://root:@localhost/flask-shopy-2687386'
     SQLALCHEMY_TRACK_MODIFICATION=False
+    SECRET_KEY = 'SHEISVERYHOT'
